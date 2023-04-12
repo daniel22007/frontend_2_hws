ccd0614ed92b8dc21e1d1a6479a56d1fe00f488e
Daniel <130543596+daniel22007@users.noreply.github.com>
2023-04-12 03:23:43 -0500

    Выполнил Дз -1

---
+++ DZ1/main.js
@@ -0,0 +1,56 @@
+var rate = prompt("Оставьте свой отзыв от 1 до 10");
+// if (rate > 11) {
+//   alert("error");
+// } else if (rate <= 5) {
+//   alert("Спасибо вам за высокую оценку!");
+// } else if (rate < 5) {
+//   alert("Мы работаем над улучшением сервиса!");
+// } else alert("ERROR!!!");
+switch (rate) {
+  case "1":
+    alert("Мы работаем над улучшением сервиса!");
+    break;
+  case "2":
+    alert("Мы работаем над улучшением сервиса!");
+    break;
+  case "3":
+    alert("Мы работаем над улучшением сервиса!");
+    break;
+  case "4":
+    alert("Мы работаем над улучшением сервиса!");
+    break;
+  case "5":
+    alert("Спасибо вам за высокую оценку!");
+    break;
+  case "6":
+    alert("Спасибо вам за высокую оценку!");
+    break;
+  case "7":
+    alert("Спасибо вам за высокую оценку!");
+    break;
+  case "8":
+    alert("Спасибо вам за высокую оценку!");
+    break;
+  case "9":
+    alert("Спасибо вам за высокую оценку!");
+    break;
+  case "10":
+    alert("Спасибо вам за высокую оценку!");
+    break;
+  default:
+    alert("ERROR!!");
+    break;
+}
+// var lang = prompt('"en"- Дни недели на английском языке' +
+//     ' "ru"- Дни недели на русском языке')
+// switch (lang) {
+//     case 'ru':
+//         console.log(['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'])
+//         break
+//     case 'en':
+//         console.log(['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'])
+//         break
+//     default:
+//         alert('ERROR')
+//         break
+// }
